0fe7e56e8be3a72091bacec613ea8f55628ab461
Tristan - Mac <sunghoonsh3@gmail.com>
2025-01-10 22:56:46 -0600

    Hompage design added

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,4 @@
 import Header from "@/app/components/Header";
-import { Geist, Geist_Mono } from "next/font/google";
 import { Lateef } from "next/font/google";
 import "./globals.css";
 
--- app/page.jsx
+++ app/page.jsx
@@ -8,16 +8,16 @@ export default function Home() {
   return (
     <div>
       <main className="p-4">
-        <div className="bg-orange-500">
+        <div className="bg-orange-500 py-52">
           {/* Inner container for hover logic */}
           <div
             onMouseEnter={() => setIsHovered(true)}
             onMouseLeave={() => setIsHovered(false)}
-            className="bg-gray-500"
+            className="bg-gray-500 flex justify-center items-center relative"
           >
             {/* Text 1: Default visible */}
             <p
-              className={`text-2xl transition-opacity duration-300 ${
+              className={`text-2xl transition-opacity duration-300 absolute ${
                 isHovered ? "opacity-0" : "opacity-100"
               }`}
             >
@@ -25,7 +25,7 @@ export default function Home() {
             </p>
             {/* Text 2: Visible on hover */}
             <p
-              className={`text-2xl font-lateef font-light top-0 left-0 transition-opacity duration-300 ${
+              className={`text-2xl font-lateef font-light transition-opacity duration-300 absolute ${
                 isHovered ? "opacity-100" : "opacity-0"
               }`}
             >
