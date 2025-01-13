c4ff4a9b33cf315ec1935ba4c6c48519b120e06a
Tristan Shin - Windows <sunghoonsh3@gmail.com>
2025-01-12 22:46:24 -0500

    Added blog preview

--- app/components/BlogPreview.jsx
+++ app/components/BlogPreview.jsx
@@ -1,27 +1,27 @@
 export default function BlogPreview({
-  blogTitle = "what i love about nd",
-  date = "Friday, December 27, 2024",
-  content = "I’ve been closely monitoring the reports of pro-Palestinian protests at various U.S. university campuses, a topic widely covered by Korean media. My interest in the Israeli-Palestinian conflict is not new; it was the subject of my History EE, focusing on the Balfour Declaration and its ongoing implications.",
-  navigation = "#",
+  title = "Default Title",
+  date = "Default Date",
+  content = "Default Content",
+  link = "#",
 }) {
   return (
-    <div className="flex justify-center gap-10 items-center font-lateef mt-[100px]">
+    <div className="flex justify-center gap-44 items-center font-lateef mt-[100px]">
       {/* Left section: Title and Date */}
-      <div className="flex flex-col text-gray-700 bg-amber-600">
-        <h2 className="font-normal text-2xl">{blogTitle}</h2>
-        <p className="text-sm">{date}</p>
+      <div className="flex flex-col">
+        <h2 className="font-normal text-2xl">{title}</h2>
+        <p className="text-sm text-descriptionGray">{date}</p>
       </div>
 
       {/* Right section: Blog Body */}
-      <div className="bg-violet-50 max-w-lg text-xl font-normal">
+      <div className="max-w-3xl text-2xl font-normal">
         <p>{content}</p>
+        <a
+          href={link}
+          className="underline decoration-underlineBlue decoration-1 underline-offset-4 mt-4 inline-block"
+        >
+          Continue reading -{">"}
+        </a>
       </div>
-      <a
-        href={navigation}
-        className="underline decoration-underlineBlue decoration-1 underline-offset-4"
-      >
-        Continue reading -{">"}
-      </a>
     </div>
   );
 }
