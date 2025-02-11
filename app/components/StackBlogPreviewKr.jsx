1d81f231f4f9a51a85f22ab07ce232cf682e4711
Tristan - Mac <sunghoonsh3@gmail.com>
2025-02-11 18:59:20 -0500

    font change for kr sectiong blog preview

--- app/components/StackBlogPreviewKr.jsx
+++ app/components/StackBlogPreviewKr.jsx
@@ -10,15 +10,15 @@ export default function StackBlogPreviewKr({
     <div className="flex flex-col lg:flex-row justify-start lg:items-start font-serif mt-10">
       {/* Left section: Title and Date */}
       <div className="flex flex-col mx-2 sm:mx-20 xl:mx-60 2xl:mx-20 mb-4 lg:mb-0">
-        <h2 className="font-semibold text-lg md:text-xl lg:text-2xl whitespace-nowrap text-wrap mb-2">
+        <h2 className="font-bold text-md md:text-lg lg:text-xl whitespace-nowrap text-wrap mb-2">
           {title}
         </h2>
-        <p className="flex font-lateef items-center text-descriptionGray text-sm sm:text-lg lg:text-lg xl:text-xl mb-2">
+        <p className="flex font-lateef items-center text-descriptionGray text-sm sm:text-md lg:text-lg xl:text-xl mb-2">
           {date}
         </p>
 
         {/* Right section: Blog Body */}
-        <div className="lg:mr-10 md:max-w-[800px] xl:max-w-[1200px] max-w-3xl text-md lg:text-lg font-normal">
+        <div className="lg:mr-10 md:max-w-[800px] xl:max-w-[1200px] max-w-3xl text-sm lg:text-md xl:text-lg font-normal">
           <p>{content}</p>
           <Link
             href={slug ? `/blog/${slug}` : "#"}
