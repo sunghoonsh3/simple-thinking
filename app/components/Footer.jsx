11a54ef78a5ffd848e721489530a237f6f3d4061
Tristan Shin <sunghoonsh3@gmail.com>
2026-01-12 11:59:28 -0500

    change year

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="flex flex-col justify-center items-center mt-[200px] mb-10 gap-5 font-lateef font-normal sm:text-2xl text-descriptionGray">
       <p>i write inspiration from books and life</p>
-      <p>© 2025 simple thinking</p>
+      <p>© 2026 simple thinking</p>
     </footer>
   );
 }
